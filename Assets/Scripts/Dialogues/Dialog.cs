5f80d5631ccbaf1cd687b71ceb4a9749dd46f613
ManuDem <thepapz7@gmail.com>
2022-01-19 17:20:36 +0100

    Improve Dialog

--- Assets/Scripts/Dialogues/Dialog.cs
+++ Assets/Scripts/Dialogues/Dialog.cs
@@ -5,9 +5,49 @@ using UnityEngine;
 [System.Serializable]
 public class Dialog
 {
+
+    [TextArea] [SerializeField] string dialog;
+
     [SerializeField] List<string> lines;
 
     public List<string> Lines {
-        get { return lines; }
+        get 
+        {
+            string[] splittedStringNewLine = dialog.Split(new char[] { '\n' });
+            for (int countNewLine = 0; countNewLine < splittedStringNewLine.Length; countNewLine++) {
+                string[] splittedStringSpace = splittedStringNewLine[countNewLine].Split(new char[] { ' ' });
+                string tempString = "";
+
+                for (int count = 0; count < splittedStringSpace.Length; count++)
+                {
+                    if (tempString == "")
+                    {
+                        tempString = splittedStringSpace[count];
+                    }
+                    else
+                    {
+                        tempString = tempString + " " + splittedStringSpace[count];
+                    }
+
+                    if (tempString.Length >= 80)
+                    {
+                        lines.Add(tempString);
+                        tempString = "";
+                    }
+                    else if (count == (splittedStringSpace.Length - 1))
+                    {
+                        lines.Add(tempString);
+                        tempString = "";
+                    }
+                }
+            }
+
+            
+            return lines;
+
+        }
     }
-}
+    }
+
+
+
