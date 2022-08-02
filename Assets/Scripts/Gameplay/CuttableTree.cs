a093976d5c574fe8f6810f577dcc8e0d1799bb0d
ManuDem <thepapz7@gmail.com>
2022-08-02 11:07:25 +0200

    Improve strings.

--- Assets/Scripts/Gameplay/CuttableTree.cs
+++ Assets/Scripts/Gameplay/CuttableTree.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 
 public class CuttableTree : MonoBehaviour, Interactable
 {
+
+    [Header("Strings")]
+    [TextArea][SerializeField] string thisTreeLooksLikeItCanBeCut;
+    [TextArea][SerializeField] string cut;
+    [TextArea][SerializeField] string should;
+    [TextArea][SerializeField] string useCut;
+    [TextArea][SerializeField] string yes;
+    [TextArea][SerializeField] string no;
+    [TextArea][SerializeField] string usedCut;
+
     public IEnumerator Interact(Transform initiator)
     {
-        yield return DialogManager.Instance.ShowDialogText("This tree looks like it can be cut");
+        yield return DialogManager.Instance.ShowDialogText($"{thisTreeLooksLikeItCanBeCut}");
 
-        var pokemonWithCut = initiator.GetComponent<PokemonParty>().Pokemons.FirstOrDefault(p => p.Moves.Any(m => m.Base.Name == "Cut" ||m.Base.Name == "Taglio" || m.Base.Name == "HM01"));
+        var pokemonWithCut = initiator.GetComponent<PokemonParty>().Pokemons.FirstOrDefault(p => p.Moves.Any(m => m.Base.Name == $"{cut}"));
 
         if (pokemonWithCut != null)
         {
             int selectedChoice = 0;
-            yield return DialogManager.Instance.ShowDialogText($"Should {pokemonWithCut.Base.Name} use cut?", 
-                choices: new List<string>() { "Yes", "No"},
+            yield return DialogManager.Instance.ShowDialogText($"{thisTreeLooksLikeItCanBeCut} {pokemonWithCut.Base.Name} {useCut}", 
+                choices: new List<string>() { $"{yes}", $"{no}" },
                 onChoiceSelected: (selection) => selectedChoice = selection);
 
             if (selectedChoice == 0)
             {
                 // Yes
-                yield return DialogManager.Instance.ShowDialogText($"{pokemonWithCut.Base.Name} used cut!");
+                yield return DialogManager.Instance.ShowDialogText($"{pokemonWithCut.Base.Name} {usedCut}");
                 gameObject.SetActive(false);
             }
         }
--- Assets/Scripts/Gameplay/SurfableWater.cs
+++ Assets/Scripts/Gameplay/SurfableWater.cs
@@ -6,6 +6,15 @@ using UnityEngine;
 
 public class SurfableWater : MonoBehaviour, Interactable, IPlayerTriggerable
 {
+    [Header("Strings")]
+    [TextArea][SerializeField] string theWaterIsDeepBlue;
+    [TextArea][SerializeField] string surf;
+    [TextArea][SerializeField] string should;
+    [TextArea][SerializeField] string useSurf;
+    [TextArea][SerializeField] string yes;
+    [TextArea][SerializeField] string no;
+    [TextArea][SerializeField] string usedSurf;
+
     bool isJumpingToWater = false;
 
     public bool TriggerRepeatedly => true;
@@ -16,21 +25,21 @@ public class SurfableWater : MonoBehaviour, Interactable, IPlayerTriggerable
         if (animator.IsSurfing || isJumpingToWater)
             yield break;
 
-        yield return DialogManager.Instance.ShowDialogText("The water is deep blue!");
+        yield return DialogManager.Instance.ShowDialogText($"{theWaterIsDeepBlue}");
 
-        var pokemonWithSurf = initiator.GetComponent<PokemonParty>().Pokemons.FirstOrDefault(p => p.Moves.Any(m => m.Base.Name == "Surf"));
+        var pokemonWithSurf = initiator.GetComponent<PokemonParty>().Pokemons.FirstOrDefault(p => p.Moves.Any(m => m.Base.Name == $"{surf}"));
 
         if (pokemonWithSurf != null)
         {
             int selectedChoice = 0;
-            yield return DialogManager.Instance.ShowDialogText($"Should {pokemonWithSurf.Base.Name} use surf?",
-                choices: new List<string>() { "Yes", "No" },
+            yield return DialogManager.Instance.ShowDialogText($"{should} { pokemonWithSurf.Base.Name} {useSurf}",
+                choices: new List<string>() { $"{yes}", $"{no}" },
                 onChoiceSelected: (selection) => selectedChoice = selection);
 
             if (selectedChoice == 0)
             {
                 // Yes
-                yield return DialogManager.Instance.ShowDialogText($"{pokemonWithSurf.Base.Name} used surf!");
+                yield return DialogManager.Instance.ShowDialogText($"{pokemonWithSurf.Base.Name} {usedSurf}");
                 
                 var dir = new Vector3(animator.MoveX, animator.MoveY);
                 var targetPos = initiator.position + dir;
